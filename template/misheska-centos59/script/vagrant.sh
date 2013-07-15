7a4a86973f1e8107244cc835ef79f2df7430abed
Mischa Taylor <mischa@misheska.com>
2013-07-15 00:55:02 -0700

    Adding definitions for CentOS 5.9 & Ubuntu 10.04

---
+++ template/misheska-centos59/script/vagrant.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eux
+
+# Vagrant specific
+date > /etc/vagrant_box_build_time
+
+yum install -y wget
+
+# Add vagrant user
+/usr/sbin/groupadd vagrant
+/usr/sbin/useradd vagrant -g vagrant -G wheel
+echo "vagrant"|passwd --stdin vagrant
+echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+
+# Installing vagrant keys
+mkdir -pm 700 /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+# Customize the message of the day
+echo 'Welcome to your Packer-built virtual machine.' > /etc/motd
+
+# Make sure udev doesn't block our network
+if grep -q -i "release 6" /etc/redhat-release ; then
+    rm /etc/udev/rules.d/70-persistent-net.rules
+    mkdir /etc/udev/rules.d/70-persistent-net.rules
+    rm /lib/udev/rules.d/75-persistent-net-generator.rules
+fi
+rm -rf /dev/.udev/
+sed -i "/^HWADDR/d" /etc/sysconfig/network-scripts/ifcfg-eth0
