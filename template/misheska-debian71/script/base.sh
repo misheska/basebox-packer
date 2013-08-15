77c5f1c8a4301169c23b1c47593a2391feaf960e
Mischa Taylor <mischa@misheska.com>
2013-08-15 01:36:30 -0700

    Adding template for Debian Wheezy 7.1.0

---
+++ template/misheska-debian71/script/base.sh
@@ -0,0 +1,16 @@
+# Disable CDROM entries
+sed -i "/^deb cdrom:/s/^/#/" /etc/apt/sources.list
+
+# Set up sudo - base careful to set the file attribue before copying to
+# sudoers.d
+( cat <<'EOP'
+%packer ALL=NOPASSWD:ALL
+EOP
+) > /tmp/packer
+chmod 0440 /tmp/packer
+mv /tmp/packer /etc/sudoers.d/
+
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install curl
+apt-get clean
