4c0f10fe7be691b1432a7a9af9d2a335672977d9
Stefan Scherer <scherer_stefan@icloud.com>
2013-09-15 17:48:02 +0200

    updated install-chocolatey.sh

--- template/stefan-win7x64/script/install-chocolatey.sh
+++ template/stefan-win7x64/script/install-chocolatey.sh
@@ -3,10 +3,7 @@ set -x
 wget http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe
 ./dotNetFx40_Full_x86_x64.exe /q /norestart
 
-wget --no-check-certificate https://github.com/ferventcoder/chocolatey/raw/master/chocolateyInstall/installChocolatey.ps1
-
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "& './installChocolatey.ps1'"
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
 
 # Cleanup
 rm -f dotNetFx40_Full_x86_x64.exe
-rm -f installChocolatey.ps1
