944812a77427d88b9728a2800602ffd3ec73dd19
Stefan Scherer <scherer_stefan@icloud.com>
2013-09-15 12:19:50 +0200

    turn on packer.log

--- make.bat
+++ make.bat
@@ -7,5 +7,8 @@ if "virtualbox" == "%FAVOR:~-11,10%" set FAVOR=virtualbox
 set TEMPLATE=%~n1
 cd template\%TEMPLATE%\
 if exist output-%FAVOR% del /S /Q /F output-%FAVOR%
+if exist output-%FAVOR% rmdir output-%FAVOR%
+set PACKER_LOG_PATH=.\packer.log
+set PACKER_LOG=1
 packer build -only=%FAVOR% template.json
 cd %CUR%
