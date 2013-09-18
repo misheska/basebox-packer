6afc1643a7e3415bd18e585b7eea8845a3824a28
Stefan Scherer <scherer_stefan@icloud.com>
2013-09-18 18:35:21 +0200

    write packer log files in log directory
    improved make.bat, started a small install readme how to set up your
    Windows host for building VMs

--- make.bat
+++ make.bat
@@ -5,18 +5,26 @@ set FAVOR=%~p1
 if "listx" == "%1x" goto list
 if "vmware" == "%FAVOR:~-7,6%" set FAVOR=vmware
 if "virtualbox" == "%FAVOR:~-11,10%" set FAVOR=virtualbox
+if "%FAVOR%" == "vmware" goto make
+if "%FAVOR%" == "virtualbox" goto make
+echo Wrong usage. Try a make list first, then make favor/template
+goto done
+
+:make
+if not exist %~dp0\log mkdir %~dp0\log
 set TEMPLATE=%~n1
 if not exist %FAVOR% mkdir %FAVOR%
 cd template\%TEMPLATE%\
 if exist output-%FAVOR% del /S /Q /F output-%FAVOR%
 if exist output-%FAVOR% rmdir output-%FAVOR%
-set PACKER_LOG_PATH=.\packer.log
+set PACKER_LOG_PATH=%~dp0\log\packer-%FAVOR%-%TEMPLATE%.log
 set PACKER_LOG=1
 packer build -only=%FAVOR% template.json
 cd %CUR%
 goto done
 
 :list
+echo Choose one of the following favor/template. Then call make favor/template
 for /F " usebackq delims==" %%i in (`dir /b template`) do @echo virtualbox/%%i
 for /F " usebackq delims==" %%i in (`dir /b template`) do @echo vmware/%%i
 :done
