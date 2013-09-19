a48d1aa0d346be170a205b2e49aa7cb859c1e82b
Stefan Scherer <scherer_stefan@icloud.com>
2013-09-19 10:07:22 +0200

    added parselog.bat to see time stamps in log

--- make.bat
+++ make.bat
@@ -11,19 +11,24 @@ echo Wrong usage. Try a make list first, then make favor/template
 goto done
 
 :make
+cd %CUR%
 if not exist %~dp0\log mkdir %~dp0\log
 set TEMPLATE=%~n1
 if not exist %FAVOR% mkdir %FAVOR%
 cd template\%TEMPLATE%\
 if exist output-%FAVOR% del /S /Q /F output-%FAVOR%
 if exist output-%FAVOR% rmdir output-%FAVOR%
-set PACKER_LOG_PATH=%~dp0\log\packer-%FAVOR%-%TEMPLATE%.log
+set LOGNAME=log\packer-%FAVOR%-%TEMPLATE%.log
+set PACKER_LOG_PATH=%~dp0\%LOGNAME%
 set PACKER_LOG=1
 packer build -only=%FAVOR% template.json
-cd %CUR%
+echo.
+echo A log file of this step could be found at %LOGNAME%
+call parselog.bat %PACKER_LOG_PATH%
 goto done
 
 :list
+cd %CUR%
 echo Choose one of the following favor/template. Then call make favor/template
 for /F " usebackq delims==" %%i in (`dir /b template`) do @echo virtualbox/%%i
 for /F " usebackq delims==" %%i in (`dir /b template`) do @echo vmware/%%i
---
+++ parselog.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+set LOGFILE=%1
+
+rem grep start time
+findstr /r "Packer.Version:" %LOGFILE%
+
+rem grep first ssh session
+for /F "usebackq delims==" %%i IN (`findstr /r opening.new.ssh.session %LOGFILE%`) DO (
+  echo %%i
+  goto sshdone
+)
+:sshdone
+
+findstr /r "VM.shut.down" %LOGFILE%
+findstr /r "Builds.completed" %LOGFILE%
+
