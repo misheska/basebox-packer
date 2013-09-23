eda502ee120ba64a226352bceaaed38fab0324b4
Stefan Scherer <scherer_stefan@icloud.com>
2013-09-23 18:18:04 +0200

    cinst packer

--- bootstrap-windows-host.bat
+++ bootstrap-windows-host.bat
@@ -2,11 +2,8 @@ powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object n
 call cinst curl
 call cinst virtualbox
 call cinst vagrant
-call cinst VirtualBox.ExtensionPack 
+rem call cinst VirtualBox.ExtensionPack 
 call cinst 7zip.commandline 
-call curl -L -k https://dl.bintray.com/mitchellh/packer/0.3.8_windows_amd64.zip -o %TEMP%\packer.zip
-cd /D %systemdrive%\chocolatey\bin
-call 7za x %TEMP%\packer.zip
-del /F %TEMP%\packer.zip
+call cinst packer
 vagrant --version
 packer --version
