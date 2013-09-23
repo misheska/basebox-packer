b27afbb46115dfa859839bbc9520c969dcfb61d2
Stefan Scherer <scherer_stefan@icloud.com>
2013-09-23 16:24:30 +0200

    download packe 0.3.8 and extract it

--- bootstrap-windows-host.bat
+++ bootstrap-windows-host.bat
@@ -4,6 +4,9 @@ call cinst virtualbox
 call cinst vagrant
 call cinst VirtualBox.ExtensionPack 
 call cinst 7zip.commandline 
-curl https://dl.bintray.com/mitchellh/packer/0.3.8_windows_amd64.zip -o %TEMP%\packer.zip
+call curl -L -k https://dl.bintray.com/mitchellh/packer/0.3.8_windows_amd64.zip -o %TEMP%\packer.zip
 cd /D %systemdrive%\chocolatey\bin
-7za -x %TEMP%\packer.zip
+call 7za x %TEMP%\packer.zip
+del /F %TEMP%\packer.zip
+vagrant --version
+packer --version
