f61a7318a85886d0e3cee2be9b5d2e6670a5fac8
Mischa Taylor <mischa@misheska.com>
2013-10-17 02:38:02 -0700

    Checksums are md5 not sha1, also use RedHat-compatible kernel by default, not the Oracle one, as it disables yum without an Oracle subscription

--- template/oraclelinux/script/kernel.sh
+++ template/oraclelinux/script/kernel.sh
@@ -1,6 +1,6 @@
 # Grub tweaks - default to RedHat-compatible kernel
 echo -n "Grub tweaks"
-sed -i 's/^default=1/default=2/' /boot/grub/grub.conf
+sed -i 's/^default=0/default=1/' /boot/grub/grub.conf
 
 # reboot
 echo "Rebooting the machine..."
