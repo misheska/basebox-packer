28ef9eab5fff1203ff061890aa2480c52e7f9a48
Mischa Taylor <mischa@misheska.com>
2013-10-18 06:55:50 -0700

    Reorganizing debian templates

--- template/debian/script/vmtools.sh
+++ template/debian/script/vmtools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-if test -f linux.iso ; then
+if [ $PACKER_BUILDER_TYPE == 'vmware' ]; then
     echo "Installing VMware Tools"
     apt-get install -y linux-headers-$(uname -r) build-essential perl
 
@@ -15,15 +15,15 @@ if test -f linux.iso ; then
 
     apt-get -y remove linux-headers-$(uname -r) build-essential perl
     apt-get -y autoremove
-elif test -f .vbox_version ; then
+elif [ $PACKER_BUILDER_TYPE == 'virtualbox' ]; then
     echo "Installing VirtualBox guest additions"
 
     apt-get install -y linux-headers-$(uname -r) build-essential perl
     apt-get install -y dkms
 
     VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-    mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
-    sh /mnt/VBoxLinuxAdditions.run
+    mount -o loop /home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso /mnt
+    sh /mnt/VBoxLinuxAdditions.run --nox11
     umount /mnt
-    rm /home/vagrant/VBoxGuestAdditions.iso
+    rm /home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso
 fi
