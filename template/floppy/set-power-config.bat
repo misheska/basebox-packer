72673b2e20596698bd912c0e1994d101a4412122
Stefan Scherer <scherer_stefan@icloud.com>
2013-10-28 21:55:21 +0100

    turn off some win8.1 cpu load tasks

--- template/floppy/set-power-config.bat
+++ template/floppy/set-power-config.bat
@@ -3,3 +3,8 @@ powercfg -setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 REM Monitor timeout
 powercfg -Change -monitor-timeout-ac 0
 powercfg -Change -monitor-timeout-dc 0
+
+rem turn off Windows 8.1 CPU load tasks
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\ProgramDataUpdater" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\AitAgent" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\StartupAppTask" /DISABLE
