eb2f2ab948a4d11dfc310c205e089b1e0e6c57b8
Mischa Taylor <mischa@misheska.com>
2013-10-29 17:07:22 -0700

    Enable docker memory and swap accounting

--- template/ubuntu/script/docker.sh
+++ template/ubuntu/script/docker.sh
@@ -8,3 +8,7 @@ apt-get update
 
 # Install, you will see another warning that the package cannot be authenticated. Confirm install.
 apt-get install -y --force-yes lxc-docker
+
+# Enable memory and swap accounting
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
+update-grub
