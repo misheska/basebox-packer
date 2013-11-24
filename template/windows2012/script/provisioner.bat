e1fc64caf0c467eff793d975368d84bfb6e14f62
Mischa Taylor <mischa@misheska.com>
2013-11-23 20:44:00 -0800

    Per Brian Flad <bflad417@gmail.com>, switching chef_version toggle to be provisioner/provisioner_version:
    * Add provisioner variable and PROVISIONER environment variable
    * Rename chef_version variable to provisioner_version
    * Rename CHEF_VERSION environment variable to PROVISIONER_VERSION
    * Rename chef.bat script to provisioner.bat
    * Use PROVISIONER to determine action in provisioner.bat

--- template/windows2012/script/provisioner.bat
+++ template/windows2012/script/provisioner.bat
@@ -2,13 +2,14 @@
 
 setlocal EnableExtensions EnableDelayedExpansion
 
-:: TEMP is not defined in this shell instance, so define it ourselves
-set LOCAL_TEMP=%USERPROFILE%\AppData\Local\Temp
-set REMOTE_SOURCE_MSI_URL=https://www.opscode.com/chef/install.msi
-set LOCAL_DESTINATION_MSI_PATH=%LOCAL_TEMP%\chef-client-latest.msi
-set FALLBACK_QUERY_STRING=?DownloadContext=PowerShell
+if "%PROVISIONER%" == "chef" (
+  :: TEMP is not defined in this shell instance, so define it ourselves
+  set LOCAL_TEMP=%USERPROFILE%\AppData\Local\Temp
+  set REMOTE_SOURCE_MSI_URL=https://www.opscode.com/chef/install.msi
+  set LOCAL_DESTINATION_MSI_PATH=%LOCAL_TEMP%\chef-client-latest.msi
+  set FALLBACK_QUERY_STRING=?DownloadContext=PowerShell
 
-if NOT "%CHEF_VERSION%" == "provisionerless" (
+  :: Always latest, for now
   echo ==^> Downloading Chef client %CHEF_VERSION%
   powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%REMOTE_SOURCE_MSI_URL%%FALLBACK_QUERY_STRING%', '%LOCAL_DESTINATION_MSI_PATH%')" <NUL
 
@@ -18,8 +19,7 @@ if NOT "%CHEF_VERSION%" == "provisionerless" (
   echo ==^> Cleaning up Chef install
   del /F /Q "%LOCAL_DESTINATION_MSI_PATH%"
 ) else (
-  echo ==^> This install is provisionerless.  Use the vagrant-omnibus plugin
-  echo ==^> to install Chef.
+  echo ==^> Building box without a provisioner."
 )
 
 endlocal
