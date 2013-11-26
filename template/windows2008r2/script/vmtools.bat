1cb8855261264d55a30cac62b09aeb519856efc0
Mischa Taylor <mischa@misheska.com>
2013-11-26 00:12:28 -0800

    Fixing EnableDelayedExpansion issues

--- template/windows2008r2/script/vmtools.bat
+++ template/windows2008r2/script/vmtools.bat
@@ -17,18 +17,18 @@ if not defined TEMP set TEMP=%USERPROFILE%\AppData\Local\Temp
 set SEVENZIP_URL=http://downloads.sourceforge.net/sevenzip/%SEVENZIP_INSTALL%
 set SEVENZIP_INSTALL_LOCAL_PATH=%TEMP%\%SEVENZIP_INSTALL%
 
-echo ==^> Downloadling %SEVENZIP_URL% to "%SEVENZIP_INSTALL_LOCAL_PATH%"
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%SEVENZIP_URL%', '"%SEVENZIP_INSTALL_LOCAL_PATH%"')" <NUL
+echo ==^> Downloadling %SEVENZIP_URL% to %SEVENZIP_INSTALL_LOCAL_PATH%
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('!SEVENZIP_URL!', '"!SEVENZIP_INSTALL_LOCAL_PATH!"')" <NUL
 echo ==^> Download complete
 echo ==^> Installing 7zip from "%SEVENZIP_INSTALL_LOCAL_PATH%"
 msiexec /qb /i "%SEVENZIP_INSTALL_LOCAL_PATH%"
 
 if "%PACKER_BUILDER_TYPE%" equ "vmware" (
   echo ==^> Extracting the VMWare Tools installer
-  "%SystemDrive%\Program Files\7-Zip\7z.exe" x %USERPROFILE%\windows.iso -o%TEMP%\vmware
+  "%SystemDrive%\Program Files\7-Zip\7z.exe" x %USERPROFILE%\windows.iso -o!TEMP!\vmware
 
   echo ==^> Installing VMware tools
-  "%TEMP%\vmware\%VMWARE_INSTALL%" /S /v "/qn REBOOT=R ADDLOCAL=ALL"
+  "%TEMP%\vmware\!VMWARE_INSTALL!" /S /v "/qn REBOOT=R ADDLOCAL=ALL"
 
   echo ==^> Cleaning up VMware tools install
   del /F /S /Q "%TEMP%\vmware"
@@ -36,16 +36,16 @@ if "%PACKER_BUILDER_TYPE%" equ "vmware" (
 
 if "%PACKER_BUILDER_TYPE%" equ "virtualbox" (
   echo ==^> Extracting the VirtualBox Guest Additions installer
-  "%SystemDrive%\Program Files\7-Zip\7z.exe" x %USERPROFILE%\VBoxGuestAdditions.iso -o%TEMP%\virtualbox
+  "%SystemDrive%\Program Files\7-Zip\7z.exe" x %USERPROFILE%\VBoxGuestAdditions.iso -o!TEMP!\virtualbox
 
   echo ==^> Installing Oracle certificate to keep install silent
   certutil -addstore -f "TrustedPublisher" a:\oracle-cert.cer
 
   echo ==^> Installing VirtualBox Guest Additions
-  "%TEMP%\virtualbox\%VBOX_INSTALL%" /S
+  "!TEMP!\virtualbox\!VBOX_INSTALL!" /S
 
   echo ==^> Cleaning up VirtualBox Guest Additions install
-  del /F /S /Q "%TEMP%\virtualbox"
+  del /F /S /Q "!TEMP!\virtualbox"
 )
 
 echo ==^> Uninstalling 7zip
