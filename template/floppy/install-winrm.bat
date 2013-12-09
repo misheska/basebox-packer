57a0219c6830531c4988364b210913371675489f
Stefan Scherer <scherer_stefan@icloud.com>
2013-12-09 20:47:46 +0100

    winrm firewall rule also for public network

--- template/floppy/install-winrm.bat
+++ template/floppy/install-winrm.bat
@@ -1,4 +1,3 @@
-powershell -NoProfile -ExecutionPolicy Bypass -File A:\ChangeCategory.ps1
 cmd /c winrm quickconfig -q
 cmd /c winrm quickconfig -transport:http # needs to be auto no questions asked
 cmd /c winrm set winrm/config @{MaxTimeoutms="1800000"}
@@ -9,6 +8,8 @@ cmd /c winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
 cmd /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
 cmd /c netsh advfirewall firewall add portopening TCP 5985 "Port 5985"
 powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-NetFirewallRule -DisplayGroup 'Windows Remote Management' -Enabled True"
+netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-In) for vagrant" description="Allow Windows Remote Management even on public networks for vagrant boxes" dir=in action=allow protocol=tcp localport=5985
+
 cmd /c net stop winrm
 cmd /c net start winrm
 cmd /c sc config "WinRM" start= auto
