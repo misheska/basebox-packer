02bfa6801d1b0caf83ecb7e1f945b197fdee6568
Mischa Taylor <mischa@misheska.com>
2014-01-08 04:12:09 -0800

    Moving nfs-utils package reference to kickstart - more reliable way of loading

--- template/oraclelinux/script/base.sh
+++ template/oraclelinux/script/base.sh
@@ -6,6 +6,3 @@ wget -O/etc/pki/tls/certs/ca-bundle.crt http://curl.haxx.se/ca/cacert.pem
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 sed -i "s/^\(.*env_keep = \"\)/\1PATH /" /etc/sudoers
-
-# Ensure NFS mounts work properly
-yum install -y nfs-utils
