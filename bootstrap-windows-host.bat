5b2533a7a9358e139e136cac7633f98d6ecb05eb
Stefan Scherer <stefan@Stefans-MacBook-Pro.local>
2014-03-08 01:27:21 +0100

    removed own code

--- bootstrap-windows-host.bat
+++
@@ -1,9 +0,0 @@
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
-call cinst curl
-call cinst virtualbox
-call cinst vagrant
-rem call cinst VirtualBox.ExtensionPack 
-call cinst 7zip.commandline 
-call cinst packer
-vagrant --version
-packer --version
