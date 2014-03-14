adb27f7d119c362474d7c2efb62ac1aace078412
Mischa Taylor <mischa@misheska.com>
2014-03-13 21:01:16 -0700

    Reformatting to match recent changes in the Mac OS X scripts and upcoming script changes for all the boxes

--- template/centos/script/provisioner.sh
+++ template/centos/script/provisioner.sh
@@ -1,63 +1,90 @@
 #!/bin/bash -eux
 
-# Bash options enabled:
-#   - Break on error
-#   - Exit if we use any unset variable (use ${VAR:-} to test for unset var)
-#   - Be verbose
-
-# Set ${PROVISIONER} & ${PROVISIONER_VERSION} inside of Packer's template.
-# if ${PROVISIONER_VERSION} isn't defined, set it to 'latest'
-PROVISIONER_VERSION=${PROVISIONER_VERSION:-latest}
-
-# Valid values for $PROVISIONER are:
+# PROVISIONER and PROVISIONER_VERSION variables should be set inside of
+# Packer's template:
+#
+# Values for PROVISIONER are:
 #   'provisionerless' -- build a box without a provisioner
 #   'chef'            -- build a box with the Chef provisioner
 #   'salt'            -- build a box with the Salt provisioner
 #   'puppet'          -- build a box with the Puppet provisioner
 #
-# When $PROVISIONER != 'provisionerless' valid options for
-# ${PROVISIONER_VERSION} are:
+# When PROVISIONER != 'provisionerless' values for PROVISIONER_VERSION are:
 #   'x.y.z'           -- build a box with version x.y.z of the Chef provisioner
 #   'x.y'             -- build a box with version x.y of the Salt provisioner
 #   'latest'          -- build a box with the latest version of the provisioner
+#
+# Assume that PROVISIONER environment variable is set inside of Packer's
+# template.
+#
+# Set PROVISIONER_VERSION to 'latest' if unset because it can be problematic
+# to set variables in pairs with Packer (and Packer does not support
+# multi-value variables).
+PROVISIONER_VERSION=${PROVISIONER_VERSION:-latest}
+
+#
+# Provisioner installs.
+#
+
+install_chef()
+{
+    echo "==> Installing Chef provisioner"
+    if [[ ${PROVISIONER_VERSION:-} == 'latest' ]]; then
+        echo "==> Installing latest Chef version"
+        curl -L https://www.opscode.com/chef/install.sh | sh
+    else
+        echo "==> Installing Chef version ${PROVISIONER_VERSION}"
+        curl -L https://www.opscode.com/chef/install.sh | sh -s -- -v ${PROVISIONER_VERSION}
+  fi
+}
+
+install_salt()
+{
+    echo "==> Installing Salt provisioner"
+    if [[ ${PROVISIONER_VERSION:-} == 'latest' ]]; then
+        echo "==> Installing latest Salt version"
+        curl -L http://bootstrap.saltstack.org | sudo sh
+    else
+        echo "==> Installing Salt version ${PROVISIONER_VERSION}"
+        curl -L http://bootstrap.saltstack.org | sudo sh -s -- git ${PROVISIONER_VERSION}
+    fi
+}
+
+install_puppet()
+{
+    echo "==> Installing Puppet provisioner"
+    REDHAT_MAJOR_VERSION=$(egrep -Eo 'release ([0-9][0-9.]*)' /etc/redhat-release | cut -f2 -d' ' | cut -f1 -d.)
+
+    echo "==> Installing Puppet Labs repositories"
+    rpm -ipv "http://yum.puppetlabs.com/puppetlabs-release-el-${REDHAT_MAJOR_VERSION}.noarch.rpm"
+
+    if [[ ${PROVISIONER_VERSION:-} == 'latest' ]]; then
+        echo "==> Installing latest Puppet version"
+        yum -y install puppet
+    else
+        echo "==> Installing Puppet version ${PROVISIONER_VERSION}"
+        yum -y install "puppet-${PROVISIONER_VERSION}"
+    fi
+}
+
+#
+# Main script
+#
 
 case "${PROVISIONER}" in
   'chef')
-    if [[ ${PROVISIONER_VERSION} == 'latest' ]]; then
-      echo "Installing latest Chef version"
-      curl -L https://www.opscode.com/chef/install.sh | sh
-    else
-      echo "Installing Chef version ${PROVISIONER_VERSION}"
-      curl -L https://www.opscode.com/chef/install.sh | sh -s -- -v ${PROVISIONER_VERSION}
-    fi
+    install_chef
     ;;
 
   'salt')
-    if [[ ${PROVISIONER_VERSION} == 'latest' ]]; then
-      echo "Installing latest Salt version"
-      wget -O - http://bootstrap.saltstack.org | sudo sh
-    else
-      echo "Installing Salt version ${PROVISIONER_VERSION}"
-      curl -L http://bootstrap.saltstack.org | sudo sh -s -- git ${PROVISIONER_VERSION}
-    fi
+    install_salt
     ;;
 
   'puppet')
-    OS_MAJRELEASE=$(egrep -Eo 'release ([0-9][0-9.]*)' /etc/redhat-release | cut -f2 -d' ' | cut -f1 -d.)
-
-    echo "Installing Puppet Labs repositories"
-    rpm -ipv "http://yum.puppetlabs.com/puppetlabs-release-el-${OS_MAJRELEASE}.noarch.rpm"
-
-    if [[ ${PROVISIONER_VERSION:-} == 'latest' ]]; then
-      echo "Installing latest Puppet version"
-      yum -y install puppet
-    else
-      echo "Installing Puppet version ${PROVISIONER_VERSION}"
-      yum -y install "puppet-${PROVISIONER_VERSION}"
-    fi
+    install_puppet
     ;;
 
   *)
-    echo "Building box without a provisioner"
+    echo "==> Building box without a provisioner"
     ;;
 esac
