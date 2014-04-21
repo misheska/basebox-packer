4bc7606e91d417786fa04449c6b1dfe0586c4cff
Mischa Taylor <misheska@getchef.com>
2014-04-21 15:21:35 -0700

    Adding Docker template for Ubuntu 14.04 (docker command is now docker.io)

--- template/ubuntu/script/docker.sh
+++ template/ubuntu/script/docker.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
-# Add the Docker repository to your apt sources list.
-echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+UBUNTU_MAJOR_VERSION=$(lsb_release -rs | cut -f1 -d .)
 
-# Update your sources
-apt-get update
+function legacy_docker_install {
+    # Add the Docker repository to your apt sources list.
+    echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 
-# Install, you will see another warning that the package cannot be authenticated. Confirm install.
-apt-get install -y --force-yes lxc-docker
+    # Update your sources
+    apt-get update
 
-# Enable memory and swap accounting
-sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
-update-grub
+    # Install, you will see another warning that the package cannot be authenticated. Confirm install.
+    apt-get install -y --force-yes lxc-docker
 
-# reboot
-echo "Rebooting the machine..."
-reboot
-sleep 60
+    # Enable memory and swap accounting
+    sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
+    update-grub
+
+    # reboot
+    echo "Rebooting the machine..."
+    reboot
+    sleep 60
+}
+
+function docker_install {
+    # Update sources
+    apt-get update
+
+    apt-get install -y docker.io
+
+    # Enable memory and swap accounting
+    sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
+    update-grub
+}
+
+if [[ $UBUNTU_MAJOR_VERSION == "12" || $UBUNTU_MAJOR_VERSION == "13" ]]
+then
+    legacy_docker_install 
+elif [[ $UBUNTU_MAJOR_VERSION == "14" ]]
+then
+    docker_install
+fi
