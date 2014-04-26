2ed89cfad4a750b8c3817c713568ff78d85b7c26
Mischa Taylor <mischa@misheska.com>
2014-04-26 08:53:08 -0700

    Seems to be no difference between Windows 2012 R2 and Windows 2012 R2 with Update Autounattend.xml

--- template/windows2012r2update/floppy/passwordchange.bat
+++
@@ -1,3 +0,0 @@
-echo ==^> Disabling automatic machine account password changes
-:: http://support.microsoft.com/kb/154501
-REG ADD "HKLM\System\CurrentControlSet\Services\Netlogon\Parameters" /v DisablePasswordChange /t REG_DWORD /d 2 /f
