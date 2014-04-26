9981cc4c7784ae2359863256aa7ba3578d36e516
Mischa Taylor <mischa@misheska.com>
2014-04-26 08:42:59 -0700

    Looks like there is no difference between the Windows 8.1 and Windos 8.1 with Update Autounattend.xml files, so just treating it like an SP update where you change iso_url to switch between either version

--- template/windows81update/floppy/_packer_config.cmd
+++
@@ -1,5 +0,0 @@
-:: uncomment the following to echo commands as they are run
-:: set PACKER_DEBUG=1
-
-:: uncomment the following to pause PACKER_PAUSE seconds after each command is run (or you press Y)
-:: set PACKER_PAUSE=3600
