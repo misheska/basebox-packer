14312ad6bd20cdebae6d6a5c05d6b9602e8251f8
Mischa Taylor <mischa@misheska.com>
2014-04-28 02:55:13 -0700

    Also need to change ERROR check per Seth Chisamore

--- template/windows81/floppy/install-winrm.cmd
+++ template/windows81/floppy/install-winrm.cmd
@@ -13,7 +13,7 @@ if exist C:\Windows\SysWOW64\cmd.exe C:\Windows\SysWOW64\cmd.exe /c powershell -
 @if errorlevel 1 echo ERROR: The previous command returned error %ERRORLEVEL%
 
 systeminfo | findstr /B /C:"OS Name" | findstr /C:"Windows 7"
-if not errorlevel 1 goto no_fixnetwork
+if errorlevel 1 goto no_fixnetwork
 
 :: see http://blogs.msdn.com/b/powershell/archive/2009/04/03/setting-network-location-to-private.aspx
 powershell -File a:\fixnetwork.ps1 <NUL
