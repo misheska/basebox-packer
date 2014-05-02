1371d1f2d1fcd1d7a831a9fac74a2bd7467e7591
Mischa Taylor <mischa@misheska.com>
2014-05-02 04:30:43 -0700

    Adding Ubuntu 14.04 desktop template

--- template/ubuntu/script/desktop.sh
+++ template/ubuntu/script/desktop.sh
@@ -3,10 +3,17 @@
 USERNAME=vagrant
 LIGHTDM_CONFIG=/etc/lightdm/lightdm.conf
 
-# Configure lightdm autologin.
-
+echo "==> Configuring lightdm autologin"
 if [ -f $LIGHTDM_CONFIG ]; then
     echo "" >> $LIGHTDM_CONFIG
     echo "autologin-user=${USERNAME}" >> $LIGHTDM_CONFIG
     echo "autologin-user-timeout=0" >> $LIGHTDM_CONFIG
+fi
+
+echo "==> Checking version of Ubuntu"
+. /etc/lsb-release
+
+if [[ $DISTRIB_RELEASE == 14.04 ]]; then
+    echo "==> Installing ubunut-desktop"
+    apt-get install -y ubuntu-desktop
 fi
