2b80d2b955fe7feb327bfd278852a7296311a5dd
Mischa Taylor <mischa@misheska.com>
2014-05-02 19:45:36 -0700

    Updating test harness stubs to be a little more realistic

--- template/oraclelinux/test/spec_helper.rb
+++ template/oraclelinux/test/spec_helper.rb
@@ -1,6 +1,4 @@
 require 'serverspec'
-require 'pathname'
-require 'net/ssh'
 
-include SpecInfra::Helper::Ssh
-include SpecInfra::Helper::DetectOS
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
--- template/windows7/test/windows_spec.rb
+++ template/windows7/test/windows_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'spec_helper'
+require 'spec_helper'
 
 describe port(22) do
   it 'should be listening on port 22' do
